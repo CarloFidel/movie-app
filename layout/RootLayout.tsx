37dd5a9e9d55dba7a8a6ed2c52b5f6407abc8f77
CarloFidel <carloftaboada@gmail.com>
2026-05-12 17:51:23 +0200

    feat:set color palet

--- layout/RootLayout.tsx
+++ layout/RootLayout.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { Outlet as Pages } from "react-router";
 
 export const RootLayout = () => {
   return (
     <>
-      <header> Este es el header de la app</header>
+      <header className='bg-primary-500 text-white font-ma-regular'> Este es el header de la app</header>
       <main>
         <Pages />
       </main>
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div> Hola desde Movie</div>
-    </>
-  );
-}
-
-export default App;
